b0108bbcb4c00f646280d16fd143429e8bfa69f0
黎锦年 <3514328912@qq.com>
2024-12-14 17:37:01 +0800

    hello world5

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
     printf("Hello world!1");
     printf("Hello world!2");
     printf("Hello world!3");
-    // printf("Hello world!4");
+    printf("Hello world!4");
+    printf("Hello world!5");
     return 0;
 }
